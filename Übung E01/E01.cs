32f610ae516fdce620b07dc77512e457b86344b2
cookieparo <101747018+cookieparo@users.noreply.github.com>
2022-04-13 20:49:43 +0200

    asd

---
+++ Übung E01/E01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Übungen
+{
+    class Übung1
+    {
+        public static void Main(string[] args)
+        {
+            Wizard Frank = new Wizard(3, 3, "hallo");
+        }
+    }
+}
+
+
+
---
+++ Übung E01/Wizard.cs
@@ -0,0 +1,16 @@
+﻿namespace Übungen;
+
+public class Wizard
+{
+    int leben;
+    int schaden;
+    string fähigkeit;
+        
+    public Wizard(int leben,int schaden,string fähigkeit)
+    {
+        this.leben = leben;
+        this.schaden = schaden;
+        this.fähigkeit = fähigkeit;
+            
+    }
+}
